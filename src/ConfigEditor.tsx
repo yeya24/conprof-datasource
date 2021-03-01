f4afaa8d12dc5ed288da97d3bf711722bbedf61b
yeya24 <yb532204897@gmail.com>
2021-03-01 00:52:43 -0500

    use grafana DatasourceHTTPSetting component
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -1,48 +1,21 @@
-import React, { ChangeEvent, PureComponent } from 'react';
+import React from 'react';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { ConprofOptions } from './types';
-import { LegacyForms } from '@grafana/ui';
-
-const { FormField } = LegacyForms;
+import { DataSourceHttpSettings } from '@grafana/ui';
 
 interface Props extends DataSourcePluginOptionsEditorProps<ConprofOptions> {}
 
-export class ConfigEditor extends PureComponent<Props> {
-  componentDidMount() {
-    const { options } = this.props;
-    options.jsonData.defaultUrl = options.url;
-  }
+export const ConfigEditor: React.FC<Props> = props => {
+  const { options, onOptionsChange } = props;
 
-  onURLChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      url: event.target.value,
-      jsonData: {
-        ...options.jsonData,
-        defaultUrl: event.target.value,
-      },
-    });
-  };
-
-  render() {
-    const { options } = this.props;
-    const { url } = options;
-
-    return (
-      <div className="gf-form-group">
-        <h3 className="page-heading">HTTP</h3>
-        <div className="gf-form">
-          <FormField
-            label="URL"
-            labelWidth={11}
-            onChange={this.onURLChange}
-            value={url || ''}
-            tooltip="Specify a complete HTTP URL (for example http://your_server:8080);"
-            placeholder="http://localhost:8080"
-          />
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    <>
+      <DataSourceHttpSettings
+        defaultUrl="http://localhost:8080"
+        dataSourceConfig={options}
+        showAccessOptions={true}
+        onChange={onOptionsChange}
+      />
+    </>
+  );
+};
